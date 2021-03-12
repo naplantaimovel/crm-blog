c9f5c8a5927044ce5bd004724f07d76ae46ced37
lucas vasconcelos <lvasconcelos802@users.noreply.github.com>
2021-03-12 01:05:25 -0300

    Area

--- crm/App_Start/RouteConfig.cs
+++ crm/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace crm
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Blog", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
